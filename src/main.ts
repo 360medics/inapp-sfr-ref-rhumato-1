f142a80c836ba285e1102892835a8defd0479fd8
Romain Billot <romain.billot@360medics.com>
2022-03-22 12:32:00 +0100

    feat: google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { createApp } from 'vue'
+import VueGtag from "vue-gtag";
 import App from './App.vue'
 import router from './router'
 import { store } from './store'
 
+// G-JVLG7EPNN6
+
 createApp(App)
   .use(router)
   .use(store)
+  .use(
+    VueGtag,
+    {
+      config: {
+        id: "G-JVLG7EPNN6",
+      },
+    },
+    router
+  )
   .mount('#app')
